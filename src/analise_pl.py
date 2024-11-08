ffa8a8e5d1e3441fca1e89d88b28a1b6626d4cc1
guilhermessantana <115511601+Globals25@users.noreply.github.com>
2024-11-08 19:52:04 -0300

    Update analise_pl.py

--- src/analise_pl.py
+++ src/analise_pl.py
@@ -9,10 +9,10 @@ import plotly.express as px
 from dash import Dash, dcc, html, Input, Output, callback
 
 from tqdm.auto import tqdm, trange
-
+from pathlib import Path
 
 # In[345]:
-
+xlsx_path = Path(__file__).parent.parent/'data'/
 
 def carrega_carteiras_pl(periodo,taxa):
     nomes_carteiras = ["Conservadora - Análise PL",
@@ -40,9 +40,9 @@ def carrega_carteiras_pl(periodo,taxa):
     
     carteiras = pd.DataFrame()
     for i in range(len(nomes_carteiras)):
-        caminho_arquivo_carteira = "C:/Users/Guilherme/Desktop/simulacoes/carteiras_pl/{}/{}/{}.xlsx".format(nomes_carteiras[i],
-                                                                                                             periodo_carteiras[periodo],
-                                                                                                             taxa_retirada[taxa])
+        caminho_arquivo_carteira = "xlsx_path/{}/{}/{}.xlsx".format(nomes_carteiras[i],
+                                                                    periodo_carteiras[periodo],
+                                                                    taxa_retirada[taxa])
         carteira = pd.read_excel(caminho_arquivo_carteira)
         carteira.drop(["Unnamed: 0"], axis = 1, inplace= True)
         carteira_tratada = carteira.iloc[-1].rename(nomes_carteiras[i].split()[0])
@@ -78,7 +78,7 @@ def carrega_carteiras_pl_dash(periodo,taxa):
 
     carteiras = pd.DataFrame()
     for i in range(len(nomes_carteiras)):
-        caminho_arquivo_carteira = "C:/Users/Guilherme/Desktop/simulacoes/carteiras_pl/OldData/{}/{}/{}.xlsx".format(nomes_carteiras[i],
+        caminho_arquivo_carteira = "xlsx_path/{}/{}/{}.xlsx".format(nomes_carteiras[i],
                                                                                                              periodo,
                                                                                                              taxa)
         carteira = pd.read_excel(caminho_arquivo_carteira)
@@ -115,7 +115,7 @@ def carrega_carteiras_retornos(carteira_idx,periodo):
     carteiras = pd.DataFrame()
     #10 Anos Conservadora - Análise PL_10 Anos
     #carteiras_pl/10 Anos/Conservadora 10 - Análise PL_10 Anos.xlsx'
-    caminho_arquivo_carteira = "C:/Users/Guilherme/Desktop/simulacoes/carteiras_pl/{}/{} {} - Análise PL_{}.xlsx".format(nomes_carteiras[carteira_idx],
+    caminho_arquivo_carteira = "xlsx_path/{}/{} {} - Análise PL_{}.xlsx".format(nomes_carteiras[carteira_idx],
                                                                                                                          periodo_carteiras[periodo],
                                                                                                                          nomes_carteiras[carteira_idx].split()[0],
                                                                                                                          periodo_carteiras[periodo])
